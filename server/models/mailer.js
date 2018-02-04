26e89c81610d95ca648e6d55d2642f7bd8326538
Alexander Kovalchuk <skateray17@gmail.com>
2018-02-04 21:54:32 +0300

    Added mailer class

--- server/models/mailer.js
+++ server/models/mailer.js
@@ -1,6 +1,37 @@
-//TODO
-class Mailer {
+const nodeMailer = require('nodemailer');
+const mailPassword = require('../secret-info/mail-password');
 
+class Mailer {
+    constructor() {
+        this.mailer = nodeMailer;
+        this.transporter = this.mailer.createTransport({
+            service: 'gmail',
+            secure: false,
+            port: 25,
+            auth: {
+                user: 'notesapptask@gmail.com',
+                pass: mailPassword,
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
+    }
+
+    sendPasswordResetHash(email, passwordResetHash) {        
+            const mailOptions = {
+                from: '"Best Notes Ever 👻" <notesapptask@gmail.com>',
+                to: email,
+                subject: 'Password changing confirmition ✔',
+                text: `To change your password enter: ${passwordResetHash}`,
+            };
+        
+            this.transporter.sendMail(mailOptions, (error) => {
+                if (error) {
+                    return console.log(error);
+                }
+            });
+    }
 }
 
 module.exports = Mailer;
